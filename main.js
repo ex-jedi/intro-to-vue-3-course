646b5f127ed500c6db4267f08a70cf8d95609421
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-13 18:03:14 +0100

    Initialising Vue accordijng to the docs

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const app = Vue.createApp({
+const app = {
   data() {
     return {
       product: 'Socks',
-      description: 'Comfy Vue Mastery Socks',
+      description: 'Comfy Vue Mastery Footgloves',
     };
   },
-});
+};
+
+// eslint-disable-next-line no-undef
+Vue.createApp(app).mount('#app');
