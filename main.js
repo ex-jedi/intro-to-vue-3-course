5ee04b7295abca37343f5541da591a3238eaff11
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-13 20:09:57 +0100

    Rendering list

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-// https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3
-
 const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      description: 'Comfy Vue Mastery socks.',
-      image: './assets/images/socks_green.jpg',
-      href: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
-      inventory: 11,
-      onSale: true,
+      image: './assets/images/socks_blue.jpg',
+      inStock: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
     };
   },
 });
