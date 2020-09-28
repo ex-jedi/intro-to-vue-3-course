56615e959382a150411499f37c5167fbda15c8d7
Mark Phoenix <mark@pixelsmiths.co.uk>
2020-09-28 08:25:53 +0100

    Finished

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 0,
+      cart: [],
       // Data to be sent to component as prop.
       premium: true,
     };
   },
   methods: {
-    updateCart() {
-      this.cart += 1;
+    updateCart(id) {
+      this.cart.push(id);
     },
   },
 });
