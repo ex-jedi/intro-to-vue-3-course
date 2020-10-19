ba4783af4b15637582a3f06e79dde6a207e16aa8
Mark <mark@pixelsmiths.co.uk>
2020-10-19 08:16:49 +0100

    Starting files

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -2,11 +2,11 @@ app.component('product-display', {
   props: {
     premium: {
       type: Boolean,
-      required: true,
-    },
+      required: true
+    }
   },
   template:
-    /* html */
+    /*html*/
     `<div class="product-display">
     <div class="product-container">
       <div class="product-image">
@@ -38,17 +38,9 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
-        <button
-          class="button"
-          :class="{ disabledButton: !inStock }"
-          :disabled="!inStock"
-          v-on:click="removeFromCart">
-          Remove from Cart
-        </button>
+
       </div>
     </div>
-    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
-    <review-form @review-submitted="addReview"></review-form>
   </div>`,
   data() {
     return {
@@ -58,40 +50,33 @@ app.component('product-display', {
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
         { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 30 },
-      ],
-      reviews: [],
-    };
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+      ]
+    }
   },
   methods: {
     addToCart() {
-      this.$emit('add-to-cart', this.variants[this.selectedVariant].id);
-    },
-    removeFromCart() {
-      this.$emit('remove-from-cart', this.variants[this.selectedVariant].id);
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
     },
     updateVariant(index) {
-      this.selectedVariant = index;
-    },
-    addReview(review) {
-      this.reviews.push(review);
-    },
+      this.selectedVariant = index
+    }
   },
   computed: {
     title() {
-      return `${this.brand} ${this.product}`;
+      return this.brand + ' ' + this.product
     },
     image() {
-      return this.variants[this.selectedVariant].image;
+      return this.variants[this.selectedVariant].image
     },
     inStock() {
-      return this.variants[this.selectedVariant].quantity;
+      return this.variants[this.selectedVariant].quantity
     },
     shipping() {
       if (this.premium) {
-        return 'Free';
+        return 'Free'
       }
-      return 2.99;
-    },
-  },
-});
+      return 2.99
+    }
+  }
+})
--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-// https://www.vuemastery.com/courses/intro-to-vue-3/components-and-props-vue3
 const app = Vue.createApp({
   data() {
     return {
       cart: [],
-      // Data to be sent to component as prop.
-      premium: true,
-    };
+      premium: true
+    }
   },
   methods: {
     updateCart(id) {
-      this.cart.push(id);
-    },
-    reduceCart(id) {
-      // this.cart.push(id);
-      const removeIt = this.cart.indexOf(id);
-      if (removeIt > -1) {
-        this.cart.splice(removeIt, 1);
-        console.log(`Removing ${removeIt}`);
-      } else {
-        console.log('Nothing to see here!');
-      }
-    },
-  },
-});
+      this.cart.push(id)
+    }
+  }
+})
