aa403b59077085fc4abc04a921a0629e1d7ebf3f
Mark <mark@pixelsmiths.co.uk>
2020-11-27 17:22:25 +0000

    Adding list of sizes with v-for

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
       image: './assets/images/socks_blue.jpg',
       inStock: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
+      sizes: ['small', 'medium', 'large'],
       variants: [
         { id: 1234, color: 'green' },
         { id: 2235, color: 'blue' },
