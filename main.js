2ed73fe961d9b00e1cdd054cd2a8a4296f907ee0
Mark <mark@pixelsmiths.co.uk>
2020-11-27 16:57:57 +0000

    V if else statement

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: 'Socks',
-      image: '/assets/images/socks_green.jpg',
-      url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
+      image: './assets/images/socks_blue.jpg',
+      inStock: false,
     };
   },
 });
