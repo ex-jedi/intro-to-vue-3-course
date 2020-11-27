56795b019bd45219107c1153521ee4c1e8463db2
Mark <mark@pixelsmiths.co.uk>
2020-11-27 16:07:08 +0000

    Restart

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-// https://www.vuemastery.com/courses/intro-to-vue-3/creating-the-vue-app-vue3/
-
-const app = Vue.createApp({
-  data() {
-    return {
-      product: 'Socks',
-      // Solution
-      description: 'Comfy Vue Mastery socks.',
-      // Solution
-    };
-  },
-});
+const product = 'Socks'
