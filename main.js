8239ac9146ec83095db9acc7169d718d340c78c5
Mark <mark@pixelsmiths.co.uk>
2020-11-27 16:48:26 +0000

    Excercise: adding link href using v-bind

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
     return {
       product: 'Socks',
       image: '/assets/images/socks_green.jpg',
+      url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
     };
   },
 });
