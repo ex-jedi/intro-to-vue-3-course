b4b3236e0f6d28b50be040772b42783922d814a5
Mark <mark@pixelsmiths.co.uk>
2020-11-27 17:33:48 +0000

    Adding Vue click event to cart

--- main.js
+++ main.js
@@ -12,19 +12,4 @@ const app = Vue.createApp({
       ],
     };
   },
-  methods: {
-    addToCart() {
-      this.cart += 1;
-    },
-    // solution
-    removeFromCart() {
-      if (this.cart >= 1) {
-        this.cart -= 1;
-      }
-    },
-    // solution
-    updateImage(variantImage) {
-      this.image = variantImage;
-    },
-  },
 });
