35a4b59d2c11e65843d2ede5a07eb7b4d27d9479
Mark <mark@pixelsmiths.co.uk>
2020-11-29 15:17:04 +0000

    Adding review form

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -1,16 +1,16 @@
-app.cpmponent('review-form', {
+app.component('review-form', {
   template:
     /* html */
-    `<form class="review-form">
+    `<form class="review-form" @submit.prevent="onSubmit">
     <h3>Leave a review</h3>
     <label for="name">Name:</label>
-    <input id="name">
+    <input id="name" v-model="name">
 
     <label for="review">Review:</label>
-    <textarea id="review"></textarea>
+    <textarea id="review" v-model="review"></textarea>
 
     <label for="rating">Rating:</label>
-    <select id="rating">
+    <select id="rating" v-model.number="rating">
       <option>5</option>
       <option>4</option>
       <option>3</option>
@@ -27,5 +27,18 @@ app.cpmponent('review-form', {
       review: '',
       rating: null,
     };
+  },
+  methods: {
+    onSubmit() {
+      const productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating,
+      };
+      this.$emit('review-submitted', productReview);
+      this.name = '';
+      this.review = '';
+      this.rating = null;
+    },
   },
 });
