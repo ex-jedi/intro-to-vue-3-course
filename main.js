06f6d81bf0e9d6c3bed0f2956baaadb33b3029d4
Mark <mark@pixelsmiths.co.uk>
2021-02-16 08:42:40 +0000

    Starter files

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      product: 'Socks',
-      image: './assets/images/socks_blue.jpg',
-      url: 'https://www.jw.org/en/bible-teachings/peace-happiness/anxiety-in-men/',
-    };
-  },
-});
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg'
+        }
+    }
+})
