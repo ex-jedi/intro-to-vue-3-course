ade6aa117b4132366bcbbcfc0d4223821dda83fe
Mark <mark@pixelsmiths.co.uk>
2021-02-16 08:17:19 +0000

    Starter files

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const app = Vue.createApp({
-  // Options object
   data() {
     return {
-      product: 'Boots',
-      description: 'Things to put on your feet',
+      product: 'Socks',
     };
   },
 });
