17a6503634bdb9cfddc2b168c045607255e61549
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-03-15 08:53:42 +0000

    Starter files

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const app = Vue.createApp({
   data() {
     return {
+      cart: 0,
       product: 'Socks',
       image: './assets/images/socks_blue.jpg',
       inStock: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
-      sizes: ['extra-small', 'small', 'medium', 'large', 'extra large'],
       variants: [
-        { id: 87, color: 'green' },
-        { id: 36, color: 'blue' },
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
       ],
     };
   },
