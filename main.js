2651be841aa158ebbb13398527320f836939429d
Mark Phoenix <mark@pixelsmiths.co.uk>
2021-03-15 08:41:44 +0000

    Adding variants

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const app = Vue.createApp({
       image: './assets/images/socks_blue.jpg',
       inStock: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 87, color: 'green' },
+        { id: 36, color: 'blue' },
+      ],
     };
   },
 });
